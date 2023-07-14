dce12269e3e797e4c25b7ea2bd68baa20c0c1c76
Lukasz Dziedziak <lukidzi@gmail.com>
2023-07-14 16:32:46 -0500

    fix(mac): remove include openssl which causes build fail macos/amd64
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- terraform/macos.tf
+++ terraform/macos.tf
@@ -15,6 +15,7 @@ sudo -u ec2-user -i <<SUDOEOF
 echo "alias python=python3" >> ~/.bash_profile
 # Using && is apparently necessary to ensure touch runs. Do not modify without testing!
 brew install automake cmake coreutils libtool wget ninja go && brew reinstall --force bazelisk && touch ~/ready
+rm -rf /usr/local/include/openssl
 SUDOEOF
 EOF
 }
