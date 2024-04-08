ec51d412380d5fea6bd355efafa976c4ca6f50d1
Mike Beaumont <mjboamail@gmail.com>
2024-04-08 16:26:11 +0200

    ci(.github): cleanup release-hosts
    Signed-off-by: Mike Beaumont <mjboamail@gmail.com>

--- terraform/release-hosts.sh
+++ terraform/release-hosts.sh
@@ -1,22 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -e -o pipefail
 
-export AWS_REGION=us-east-2
-hostIDs=$(aws ec2 describe-hosts \
+describeHosts=$(aws ec2 describe-hosts \
     --filter \
         Name=tag-key,Values=EnvoyCI \
         Name=state,Values=available \
     | jq -r '.Hosts[].HostId'
 )
 
-arr=(${hostIDs//$'\n'/ })
-code=0
-for value in "${arr[@]}"
-do
-  hostCleaned=$(aws ec2 release-hosts --host-ids=$value --no-cli-pager | jq -r '.Unsuccessful == []')
-  if [[ hostCleaned != true ]]; then
-    code=1
-  fi
+mapfile -t hostIDs <<< "${describeHosts}"
+
+exitCode=0
+for hostID in "${hostIDs[@]}"; do
+    releaseResponse=$(aws ec2 release-hosts --host-ids="${hostID}" --no-cli-pager)
+    if ! jq '(.Successful | length == 1) or (.Unsuccessful | length == 1 and .[0].Error.Code == "Client.HostMinAllocationPeriodUnexpired")' <<< "${releaseResponse}" >/dev/null; then
+        echo "Unexpected failure with ${hostID}: ${releaseResponse}"
+        exitCode=1
+    fi
 done
 
-exit $code
+exit "${exitCode}"
