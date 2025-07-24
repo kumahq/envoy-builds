2a05556b46bc4d63ea08d653c074b8529a378038
Lukasz Dziedziak <lukidzi@gmail.com>
2025-07-24 12:07:33 -0500

    fix(build): upgrade debian version and cleanup old patches (#137)
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- scripts/fetch_sources.sh
+++ scripts/fetch_sources.sh
@@ -11,21 +11,12 @@ set -o pipefail
 set -o nounset
 
 declare -A patches_per_version
-patches_per_version[v1.27]="$(realpath "patches/v1.27-0001-dns-don-t-error-if-header-id-is-0.patch")"
-patches_per_version[v1.28]="$(realpath "patches/v1.28-0001-dns-don-t-error-if-header-id-is-0.patch")"
-patches_per_version[v1.29]="$(realpath "patches/v1.29-0001-dns-don-t-error-if-header-id-is-0.patch")"
-patches_per_version[v1.30]="$(realpath "patches/v1.30-0001-dns-don-t-error-if-header-id-is-0.patch")"
 patches_per_version[v1.31]="$(realpath "patches/v1.31-0001-dns-don-t-error-if-header-id-is-0.patch")"
 patches_per_version[v1.32]="$()"
 patches_per_version[v1.33]="$()"
 patches_per_version[v1.34]="$()"
 patches_per_version[v1.35]="$()"
-
-PATCH_FILES_1_26=(
-  "$(realpath "scripts/dns_filter_resolver.h.patch")"
-  "$(realpath "scripts/filter_test.cc.patch")"
-  "$(realpath "scripts/rbac_filter.cc.patch")"
-)
+patches_per_version[v1.36]="$()"
 
 DARWIN_PATCH_FILE=$(realpath "scripts/luajit.patch.patch")
 
@@ -51,14 +42,9 @@ echo "ENVOY_TAG=${ENVOY_TAG}"
 
 echo "Checking for patches"
 
-if [[ "${ENVOY_TAG}" == "v1.26"* ]]; then
-  echo "Applying patches for Envoy 1.26"
-  git apply -v "${PATCH_FILES_1_26[@]}"
-else
-  if [[ "${GOOS}" == "darwin" ]]; then
-    echo "Applying patches for Darwin"
-    git apply -v "${DARWIN_PATCH_FILE}"
-  fi
+if [[ "${GOOS}" == "darwin" ]]; then
+  echo "Applying patches for Darwin"
+  git apply -v "${DARWIN_PATCH_FILE}"
 fi
 
 IFS=. read -r major minor rest <<< "$(cat VERSION.txt)"
