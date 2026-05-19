f4e825190c709ac1b8dcb4307cbdc254c24fab2d
Lukasz Dziedziak <lukidzi@gmail.com>
2026-05-19 07:55:33 -0500

    fix(build): image rename (#192)
    * fix(build): image rename
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>
    
    * fix
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>
    
    * test
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>
    
    * fix
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>
    
    * fix
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>
    
    ---------
    
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- scripts/build_centos.sh
+++ scripts/build_centos.sh
@@ -26,9 +26,14 @@ popd
 BUILD_CMD=${BUILD_CMD:-"bazel build ${BAZEL_BUILD_OPTIONS[@]} -c opt ${BUILD_TARGET} ${CONTRIB_ENABLED_ARGS} --//source/extensions/transport_sockets/tcp_stats:enabled=false"}
 
 ENVOY_BUILD_CONFIG=$(curl --fail --location --silent https://raw.githubusercontent.com/envoyproxy/envoy/"${ENVOY_TAG}"/.github/config.yml)
+ENVOY_BUILD_REPO=$(echo "${ENVOY_BUILD_CONFIG}" | awk '/^  repo:/ {print $2; exit}')
 ENVOY_BUILD_TAG=$(echo "${ENVOY_BUILD_CONFIG}" | awk '/^  tag:/ {print $2; exit}')
 ENVOY_BUILD_SHA=$(echo "${ENVOY_BUILD_CONFIG}" | awk '/^  sha:/ {print $2; exit}')
-ENVOY_BUILD_IMAGE="envoyproxy/envoy-build-ubuntu:${ENVOY_BUILD_TAG}@sha256:${ENVOY_BUILD_SHA}"
+# v1.37+ split envoy-build into variants; "ci" is the standard build image.
+if [[ "${ENVOY_BUILD_REPO}" != *envoy-build-ubuntu ]]; then
+  ENVOY_BUILD_TAG="ci-${ENVOY_BUILD_TAG}"
+fi
+ENVOY_BUILD_IMAGE="${ENVOY_BUILD_REPO}:${ENVOY_BUILD_TAG}@sha256:${ENVOY_BUILD_SHA}"
 LOCAL_BUILD_IMAGE="envoy-builder:${ENVOY_TAG}"
 
 docker build -t "${LOCAL_BUILD_IMAGE}" --progress=plain \
